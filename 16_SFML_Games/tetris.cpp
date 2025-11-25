e48318caeca7593a4a1f38d45df6d738126f7a16
(Student  C00313609) Kevin Michael <C00313609@setu.ie>
2025-11-25 12:19:08 +0000

    Reorganized the code into separate functions

--- 16_SFML_Games/tetris.cpp
+++ 16_SFML_Games/tetris.cpp
@@ -9,6 +9,16 @@ const int WIDTH = 10;
 const int MAX_BLOCKS = 4;
 const int NO_OF_FIGURES = 7;
 
+//Global variables
+
+int moveInX; 
+bool shouldRotate; 
+int colorNum;
+float timer;
+float delay;
+Texture tilesTexture, backgroundTexture, frameTexture;
+Sprite tile, background, frame;
+
 int field[HEIGHT][WIDTH] = {0};
 
 struct Point
@@ -24,6 +34,25 @@ int figures[NO_OF_FIGURES][MAX_BLOCKS] =
     3,5,7,6, // J
     2,3,4,5, // O
 };
+
+void initializeGame()
+{
+    //srand(time(0));
+
+    tilesTexture.loadFromFile("images/tetris/tiles.png");
+    backgroundTexture.loadFromFile("images/tetris/background.png");
+    frameTexture.loadFromFile("images/tetris/frame.png");
+
+    tile.setTexture(tilesTexture);
+    background.setTexture(backgroundTexture);
+    frame.setTexture(frameTexture);
+
+    moveInX = 0;
+    shouldRotate = 0;
+    colorNum = 1;
+    timer = 0;
+    delay = 0.3;
+}
 
 bool check()
 {
@@ -46,22 +75,109 @@ void updateActual()
         
 }
 
+void move()
+{
+    for (int i = 0; i < MAX_BLOCKS; i++) 
+    { 
+        placeHolder[i] = actual[i]; actual[i].x += moveInX; 
+    }
+    updateActual();
+}
+
+void rotate()
+{
+    if (shouldRotate)
+    {
+        Point point = actual[1]; //center of rotation
+        for (int i = 0; i < MAX_BLOCKS; i++)
+        {
+            int deltaX = actual[i].y - point.y;
+            int deltaY = actual[i].x - point.x;
+            actual[i].x = point.x - deltaX;
+            actual[i].y = point.y + deltaY;
+        }
+        updateActual();
+    }
+}
+
+void tick()
+{
+    if (timer > delay)
+    {
+        for (int i = 0; i < MAX_BLOCKS; i++)
+        {
+            placeHolder[i] = actual[i]; actual[i].y += 1;
+        }
+
+        if (!check())
+        {
+            for (int i = 0; i < MAX_BLOCKS; i++)
+            {
+                field[placeHolder[i].y][placeHolder[i].x] = colorNum;
+            }
+
+            colorNum = 1 + rand() % 7;
+            int randomNumber = rand() % 7;
+            for (int i = 0; i < MAX_BLOCKS; i++)
+            {
+                actual[i].x = figures[randomNumber][i] % 2;
+                actual[i].y = figures[randomNumber][i] / 2;
+            }
+        }
+
+        timer = 0;
+    }
+}
+void chceckLines()
+{
+    int indexFromTop= HEIGHT - 1;
+        for (int i= HEIGHT -1;i>0;i--)
+        {
+            int count=0;
+            for (int j=0;j< WIDTH;j++)
+            {
+                if (field[i][j]) count++;
+                    field[indexFromTop][j]=field[i][j];
+            }
+            if (count< WIDTH) indexFromTop--;
+        }
+}
+void draw(RenderWindow &t_window)
+{
+    t_window.clear(Color::White);
+    t_window.draw(background);
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (field[i][j] == 0) continue;
+            tile.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+            tile.setPosition(j * 18, i * 18);
+            tile.move(28, 31); //offset
+            t_window.draw(tile);
+        }
+    }
+    for (int i = 0; i < MAX_BLOCKS; i++)
+    {
+        tile.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+        tile.setPosition(actual[i].x * 18, actual[i].y * 18);
+        tile.move(28, 31); //offset
+        t_window.draw(tile);
+    }
+
+    t_window.draw(frame);
+    t_window.display();
+}
+
 
 int tetris()
 {
-    srand(time(0));     
+    srand(time(0));
 
     RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "The Game!");
 
-    Texture tilesTexture,backgroundTexture,frameTexture;
-    tilesTexture.loadFromFile("images/tetris/tiles.png");
-    backgroundTexture.loadFromFile("images/tetris/background.png");
-    frameTexture.loadFromFile("images/tetris/frame.png");
-
-    Sprite tile(tilesTexture), background(backgroundTexture), frame(frameTexture);
-
-    int moveInX=0; bool shouldRotate=0; int colorNum=1;
-    float timer=0,delay=0.3; 
+    initializeGame();
 
     Clock clock;
 
@@ -86,85 +202,22 @@ int tetris()
         if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
 
         //// <- Move -> ///
-        for (int i=0;i<MAX_BLOCKS;i++)  { placeHolder[i]= actual[i]; actual[i].x+= moveInX; }
-        updateActual();
-
+        move();
+        
         //////Rotate//////
-        if (shouldRotate)
-        {
-            Point point = actual[1]; //center of rotation
-            for (int i=0;i<MAX_BLOCKS;i++)
-            {
-                int deltaX = actual[i].y- point.y;
-                int deltaY = actual[i].x- point.x;
-                actual[i].x = point.x - deltaX;
-                actual[i].y = point.y + deltaY;
-            }
-            updateActual();
-        }
-
-
+        rotate();
 
         ///////Tick//////
-        if (timer>delay)
-        {
-            for (int i=0;i<MAX_BLOCKS;i++) { placeHolder[i]= actual[i]; actual[i].y+=1; }
-
-            if (!check())
-            {
-                for (int i=0;i<MAX_BLOCKS;i++) field[placeHolder[i].y][placeHolder[i].x]=colorNum;
-
-                colorNum=1+rand()%7;
-                int randomNumber=rand()%7;
-                for (int i=0;i<MAX_BLOCKS;i++)
-                {
-                    actual[i].x = figures[randomNumber][i] % 2;
-                    actual[i].y = figures[randomNumber][i] / 2;
-                }
-            }
-
-            timer=0;
-        }
+        tick();
 
         ///////check lines//////////
-        int indexFromTop= HEIGHT - 1;
-        for (int i= HEIGHT -1;i>0;i--)
-        {
-            int count=0;
-            for (int j=0;j< WIDTH;j++)
-            {
-                if (field[i][j]) count++;
-                    field[indexFromTop][j]=field[i][j];
-            }
-            if (count< WIDTH) indexFromTop--;
-        }
+        chceckLines();
 
-        moveInX =0; shouldRotate =0; delay=0.3;
+        moveInX = 0; shouldRotate = 0; delay = 0.3;
 
         /////////draw//////////
-        window.clear(Color::White);    
-        window.draw(background);
-          
-        for (int i=0;i< HEIGHT;i++)
-        for (int j=0;j< WIDTH;j++)
-        {
-            if (field[i][j]==0) continue;
-            tile.setTextureRect(IntRect(field[i][j]*18,0,18,18));
-            tile.setPosition(j*18,i*18);
-            tile.move(28,31); //offset
-            window.draw(tile);
-        }
-
-        for (int i=0;i<MAX_BLOCKS;i++)
-        {
-            tile.setTextureRect(IntRect(colorNum*18,0,18,18));
-            tile.setPosition(actual[i].x*18, actual[i].y*18);
-            tile.move(28,31); //offset
-            window.draw(tile);
-        }
-
-        window.draw(frame);
-        window.display();
+        draw(window);
+        
     }
 
     return 0;
